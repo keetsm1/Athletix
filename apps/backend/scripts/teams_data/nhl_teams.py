c56a1e0bef5b24038e87523f421305bc6d0a44a4
Gunkeet Mutiana <145614621+keetsm1@users.noreply.github.com>
2026-07-02 16:12:06 -0400

    NHL teams now in database, working on nba teams now

--- apps/backend/scripts/teams_data/nhl_teams.py
+++ apps/backend/scripts/teams_data/nhl_teams.py
@@ -1,8 +1,31 @@
 import requests
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+from components.supabase import supabase
 
-url = https://api-web.nhle.com/
+url = 'https://api-web.nhle.com/v1/standings/2026-04-17'
 
 response = requests.get(url)
 
 if response.status_code == 200:
+     data = response.json()
+else:
+     print("Failed to get data")
+
+
+teams = []
+
+for team in data["standings"]:
+     teams.append({
+          "league": "NHL",
+          "name": team["teamName"]["default"],
+          "city": team["placeName"]["default"],
+          "abbreviation": team["teamAbbrev"]["default"],
+          "logo_url": team["teamLogo"]
+     })
+
+result = supabase.table("teams").insert(teams).execute()
+
+
     
